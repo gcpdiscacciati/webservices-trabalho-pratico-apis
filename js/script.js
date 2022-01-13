fb4775bdd5a22b80c24969f5ee31e87ba72635d3
Giovanne Discacciati <gi_discacciati@hotmail.com>
2022-01-13 07:25:42 -0300

    add attribute worldCopyJump to map

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var mapa = L.map('map', {
 	preferCanvas: true,
     zoomSnap: 0.1,
+    worldCopyJump: true,
 }).setView([15,0], 2.9);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
